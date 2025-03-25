016d3c36e38692af799066d9ec9271d436867dc7
Zahra El Baba <zahrababa73@hotmail.com>
2025-03-25 17:03:00 +0200

    categories

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import CategoryPage from "./pages/CategoryPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/category/:categoryId" element={<CategoryPage />} />
+        <Route path="*" element={<Navigate to="/category/1" />} />
+      </Routes>
+    </Router>
   );
 }
 
