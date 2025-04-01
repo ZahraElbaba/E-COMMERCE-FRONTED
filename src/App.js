71fb4ab6e0c12e69bbf56c7e78c02b89f07f7170
Zahra El Baba <zahrababa73@hotmail.com>
2025-04-01 23:20:42 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import CategoryPage from "./pages/CategoryPage";
-import CartPage from "./pages/CartPage"; 
+
 import Header from "./components/Header";
 import Footer from "./components/Footer"; // Ensure Footer is included
 //import checkout from "./components/checkout"
@@ -10,7 +10,7 @@ function App() {
       <Header /> 
       <Routes>
         <Route path="/category/:categoryId" element={<CategoryPage />} />
-        <Route path="/cart" element={<CartPage />} />
+\
         <Route path="/checkout" element={<checkout />} />
 
         
